71a34904230fa7faa0457eb891d9da2b2c7aa958
Andrey Holod <andrejholod77@gmail.com>
2024-05-20 15:20:40 +0400

    "init_3"

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const app = express();
 
 app.get("/", function (request, response) {
-    response.send("<h2>Привет Express!</h2>");
+    response.send("<h2>Привет Express!!!!!!!!!!!</h2>");
 });
 
-app.listen(port,  "0.0.0.0", () => {
-    console.log(`Server has been started`)
+app.listen(port, () => {
+    console.log(`Server has been started Y`)
 });
